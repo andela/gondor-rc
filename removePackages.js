58d0fef9f1590c19052549f1968c21370c2dda62
Stylll <stephen.aribaba@yahoo.com>
2018-08-05 08:09:22 +0100

    chore(pipeline): setup heroku pipeline and circleci - configure continous integration with circleci - setup deployment pipeline on heroku [Finishes #159431241]

---
+++ removePackages.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+var mongoClient = require('mongodb').MongoClient;
+
+if (process.env.HEROKU_APP_NAME) {
+  const url = process.env.MONGODB_URI || "";
+
+  mongoClient.connect(url, (err, db) => {
+    console.log("Mongo client connected!");
+    if (err) throw err;
+    const packages = db.collection("Packages");
+    packages.remove((error, result) => {
+      if (error) throw error;
+      console.log("Packages Collection wiped!");
+      db.close();
+    });
+  });
+}
+
