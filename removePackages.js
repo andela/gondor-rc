1861274ef0c06aa753025b093882c2e306c47708
Stylll <stephen.aribaba@yahoo.com>
2018-08-07 11:00:23 +0100

    chore(pipeline): setup heroku pipeline and circleci - add post deploy scripts and function [Finishes #159431241]

--- removePackages.js
+++ removePackages.js
@@ -4,6 +4,10 @@ var mongoClient = require('mongodb').MongoClient;
 if (process.env.HEROKU_APP_NAME) {
   const url = process.env.MONGODB_URI || "";
 
+  /**
+   * connect to the db
+   * clear the packages collection in the db
+   */
   mongoClient.connect(url, (err, db) => {
     console.log("Mongo client connected!");
     if (err) throw err;
