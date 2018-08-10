05a4e99a727b0abca6cf71edf9faf865488db26b
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-08-10 17:06:54 +0100

    feature(Design) : Redesign landing page - Create mock up for proposed designs - Convert approved design to  html and css - Implement new design  into RC [finishes #159501217]

---
+++ imports/plugins/custom/gdr-landing-page/client/components/Slider.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Slider from "react-slick";
+import { Components } from "@reactioncommerce/reaction-components";
+
+class CustomSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoPlay: true,
+      autoPlaySpeed: 1000,
+      pauseOnHover: true,
+      adaptiveHeight: true,
+      arrows: true,
+      centerMode: true,
+      className: "gdr-slick-slider"
+    };
+
+    // Get at least ten latest products for new arrivals
+    const newArrivals = this.props.products.slice(0, 11);
+
+    return (
+      <div className="gdr-slider">
+        <h2 className="gdr-landingPage-title">New Arrivals</h2>
+        <Slider {...settings}>
+          {newArrivals.map((product, key) => {
+            return (
+              <div className="gdr-slider-container" key={key}>
+                <Components.DragDropProvider key={key}>
+                  <Components.ProductGridItems
+                    {...this.props}
+                    product={product}
+                    key={key}
+                    index={key}
+                  />
+                </Components.DragDropProvider>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
+}
+
+CustomSlider.propTypes = {
+  products: PropTypes.array
+};
+
+export default CustomSlider;
---
+++ imports/plugins/custom/gdr-landing-page/client/components/WelcomeSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const WelcomeSection = () => (
+  <div className="container-fluid showcase">
+    <div className="txt-container">
+      <h1 className="first-txt">
+          We heard you like quality...
+      </h1>
+      <div className="sec-txt">
+          You are in the right place!
+      </div>
+    </div>
+  </div>
+);
+
+export default WelcomeSection;
