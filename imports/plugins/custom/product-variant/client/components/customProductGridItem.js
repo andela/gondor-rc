20979ca5b3303975f42556995508b171601e405e
Adekunle Oluwafemi <adekunleoluwafemi@Andelas-Mac.local>
2018-08-16 08:18:28 +0100

    feature(share-social): share products on social media -add share icon to product card - add event to trigger icon visibility - add social media icons - add events to trigger share for each icon [finishes #159361812]

--- imports/plugins/custom/product-variant/client/components/customProductGridItem.js
+++ imports/plugins/custom/product-variant/client/components/customProductGridItem.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, WhatsappShareButton, WhatsappIcon } from "react-share";
 import { formatPriceString } from "/client/api";
 import { replaceComponent, getRawComponent, withIsAdmin, withIsOwner } from "@reactioncommerce/reaction-components";
 
 const ProductGridItem = getRawComponent("ProductGridItems");
 
 class CustomProductGridItem extends ProductGridItem {
+  constructor() {
+    super();
+    this.state = {
+      isClicked: false
+    };
+  }
+  showIcons = () => {
+    this.setState({
+      isClicked: !this.state.isClicked
+    });
+  }
   renderRatingStars() {
     return (<div className="stars">
       <i className="fa fa-star" />
@@ -67,6 +79,83 @@ class CustomProductGridItem extends ProductGridItem {
         </a>
       </div>
     );
+  }
+
+  handleMouseEnter = () => this.socialShare.classList.add("slideIn");
+
+  handleMouseLeave = () => this.socialShare.classList.remove("slideIn");
+
+  render() {
+    const productItem = (
+      <li
+        className={`product-grid-item ${this.renderPinned()} ${this.props.weightClass()} ${this.props.isSelected()}`}
+        data-id={this.props.product._id}
+        id={this.props.product._id}
+      >
+        <div
+          className={this.renderHoverClassName()}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+        >
+          <span className="product-grid-item-alerts" />
+
+          <a className="product-grid-item-images"
+            href={this.props.pdpPath()}
+            data-event-category="grid"
+            data-event-label="grid product click"
+            data-event-value={this.props.product._id}
+            onDoubleClick={this.handleDoubleClick}
+            onClick={this.handleClick}
+          >
+            <div className={`product-primary-images ${this.renderVisible()}`}>
+              {this.renderMedia()}
+              {this.renderOverlay()}
+            </div>
+
+            {this.renderAdditionalMedia()}
+          </a>
+          <div className="social-share"
+            ref={elm => this.socialShare = elm}
+
+          >
+            <div className="share-icon-container">
+              <i onClick={this.showIcons} className="fa fa-share-alt" />
+            </div>
+            <div className={this.state.isClicked ? "social-icon-container" : "social-icon-container hide"}>
+              <div className="social-icons">
+                <div className="social-icon">
+                  <FacebookShareButton url={`${location.origin}${this.props.pdpPath()}`}>
+                    <FacebookIcon size={32} round={true} />
+                  </FacebookShareButton>
+                </div>
+                <div className="social-icon">
+                  <TwitterShareButton url={`${location.origin}${this.props.pdpPath()}`}>
+                    <TwitterIcon size={32} round={true} />
+                  </TwitterShareButton>
+                </div>
+                <div className="social-icon">
+                  <WhatsappShareButton url={`${location.origin}${this.props.pdpPath()}`}>
+                    <WhatsappIcon size={32} round={true} />
+                  </WhatsappShareButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          {!this.props.isSearch && this.renderNotices()}
+          {this.renderGridContent()}
+        </div>
+      </li>
+    );
+
+    if (this.props.canEdit) {
+      return (
+        this.props.connectDropTarget(
+          this.props.connectDragSource(productItem)
+        )
+      );
+    }
+
+    return productItem;
   }
 }
 
